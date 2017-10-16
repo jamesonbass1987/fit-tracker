a14c52c4f5a27293f9c72498ca9436b5727a52cd
jamesonbass1987 <jamesonbass@gmail.com>
2017-10-16 16:37:33 -0700

    set create functionality working'

--- app/controllers/exercise_sets_controller.rb
+++ app/controllers/exercise_sets_controller.rb
@@ -28,8 +28,35 @@ class ExerciseSetsController < ApplicationController
     if (!@exercise.user_id.nil? && @exercise.user_id != @user.id)
       redirect to :"/users/<%= @user.username %>"
     end
-    erb :'/exercise_sets/edit'
+
+    erb :'/exercise_sets/create'
   end
+
+  post '/exercises/:exercise_id/sets' do
+
+    #check for blank values
+    if params[:weight] == "" || params[:reps] == ""
+      redirect to '/exercises/:exercise_id/sets/new'
+    end
+
+    #set a set variable based on input params and save to DB
+    @set = ExerciseSet.new(:weight => params[:weight], :measurement => params[:measurement], :reps => params[:reps])
+    @set.save
+
+    #set variables for user & exercise
+    @user = current_user
+    @exercise = Exercise.find_by_id(params[:exercise_id])
+
+    #assign set to user and exercise
+    @user.exercise_sets << @set
+    @exercise.exercise_sets << @set
+
+    binding.pry
+
+    redirect to "/exercises/#{@exercise.id}"
+
+  end
+
 
 
 
